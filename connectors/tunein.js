f826fed91bf564fd93bad99374b88db0abb10981
xyauhideto <xyauhideto@gmail.com>
2017-10-20 17:45:45 +0200

    Tunein.fm: Add embed player; filter default art

--- connectors/tunein.js
+++ connectors/tunein.js
@@ -1,9 +1,40 @@
 'use strict';
 
-Connector.playerSelector = '.player__playerContainer___JEJ2U';
+setupConnector();
 
-Connector.artistTrackSelector = '#playerTitle';
+function setupConnector() {
+    if (isEmbedPlayer()) {
+        setupEmbedPlayer();
+    } else {
+        setupSitePlayer();
+    }
+    setupCommonProperties();
+}
 
-Connector.trackArtSelector = '#playerArtwork';
+function isEmbedPlayer() {
+    return $('#tuner, #mini-tuner').length > 0;
+}
 
-Connector.isPlaying = () => $('#playerActionButton').hasClass('playing');
+function setupEmbedPlayer() {
+    Connector.playerSelector = 'div[id*="tuner"]';
+
+    Connector.artistTrackSelector = `${Connector.playerSelector} .line1`;
+
+    Connector.playButtonSelector = '.stopped, .idle, .buffering';
+}
+
+function setupSitePlayer() {
+    Connector.playerSelector = '.player__playerContainer___JEJ2U';
+
+    Connector.artistTrackSelector = '#playerTitle';
+
+    Connector.playButtonSelector = '.stopped, #playerLoadingSpinner';
+}
+
+function setupCommonProperties() {
+    $('audio').bind('playing pause timeupdate', Connector.onStateChanged);
+
+    Connector.trackArtSelector = `${Connector.playerSelector} img`;
+
+    Connector.isTrackArtDefault = (trackArtUrl) => !trackArtUrl.includes('album');
+}
--- tests/connectors/tunein.js
+++ tests/connectors/tunein.js
@@ -4,4 +4,10 @@ module.exports = function(driver, connectorSpec) {
 	connectorSpec.shouldLoadWebsite(driver, {
 		url: 'https://tunein.com/radio/Classic-Rock-Hits-s249994/'
 	});
+
+    // Embed player, size options are large, medium, and small
+    // also aplied for .../embed/player/s${stationNumber}
+	connectorSpec.shouldContainPlayerElement(driver, {
+		url: 'https://tunein.com/embed/station/137689/?size=large&autoplay=true'
+	});
 };
